d8a29c403f09d91562dcf7bdca35c20ca558e7a0
Oscar Pineda <friedchicken@Oscars-MacBook-Pro.local>
2022-04-20 01:45:53 -0500

    blog entry

---
+++ 593699e4.6c45eaf8.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{148:function(o){o.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"cloud-security","name":"cloud security","count":1,"permalink":"/blog/tags/cloud-security"}')}}]);

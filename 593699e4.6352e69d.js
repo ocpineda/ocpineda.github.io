26d00ca7b4bacd6c1036cb62fd4981edf4652ee8
Oscar Pineda <friedchicken@Oscars-MacBook-Pro.local>
2022-05-06 21:49:01 -0500

    updating blog

---
+++ 593699e4.6352e69d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{145:function(o){o.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"cloud-security","name":"cloud security","count":2,"permalink":"/blog/tags/cloud-security"}')}}]);
---
+++ dfad702b.db10f55c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{165:function(a){a.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"cis-benchmark","name":"cis benchmark","count":1,"permalink":"/blog/tags/cis-benchmark"}')}}]);
